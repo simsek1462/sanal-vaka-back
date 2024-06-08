403ccbaef1c74c34e990bc25062fbc4340f7d1f6
mustafasimsk <mustafa.simsek@venhancer.com>
2024-06-08 21:16:37 +0300

    update: get steps by department id

--- src/controllers/departmentController.js
+++ src/controllers/departmentController.js
@@ -15,15 +15,24 @@ exports.createDepartment = async (req, res) => {
 exports.getStepsByDepartmentId = async (req, res) => {
   try {
     const { id } = req.params;
-    const department = await Department.findById(id).populate('testSteps');
+    const department = await Department.findById(id).populate({
+      path: 'testSteps',
+      populate: {
+        path: 'type',
+        model: 'ComponentType'
+      }
+    });
+    
     if (!department) {
       return res.status(404).json({ error: 'Department not found' });
     }
+    // const filteredSteps = department.testSteps.filter(step => step.type.unique_int !== 0);
     res.status(200).json(department.testSteps);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
+
 
 exports.getAllDepartments = async (req, res) => {
   try {
