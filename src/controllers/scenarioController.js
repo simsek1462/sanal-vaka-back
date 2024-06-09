1bb22c2fc66ca0019a3916e635b453a359a07e6e
DevBalek <muhammedfurkanbalek@gmail.com>
2024-06-09 22:34:36 +0300

    feat: check answer question

--- src/controllers/scenarioController.js
+++ src/controllers/scenarioController.js
@@ -2,19 +2,19 @@ const Question = require('../models/question');
 const Scenario = require('../models/scenario');
 
 exports.createScenario = async (req, res) => {
-    try {
-        const scenario = new Scenario(req.body);
-        await scenario.save();
-        res.status(201).json(scenario);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
+  try {
+    const scenario = new Scenario(req.body);
+    await scenario.save();
+    res.status(201).json(scenario);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 exports.checkAnswer = async (req, res) => {
   try {
     const { scenarioId, questionId } = req.body;
-    const q=await Question.findById(questionId);
+    const q = await Question.findById(questionId);
     const scenario = await Scenario.findById(scenarioId);
     if (!scenario) {
       return res.status(404).json({ error: 'Scenario not found' });
@@ -24,84 +24,84 @@ exports.checkAnswer = async (req, res) => {
     if (!question) {
       return res.status(404).json({ error: 'Yanlış soru' });
     }
-    const answer=question.answer;
-    const title=q.title;
-    const description=q.question;
-   
-    res.status(200).json({ title,description,answer });
+    const answer = question.answer;
+    const title = q.title;
+    const description = q.question;
+
+    res.status(200).json({ title, question: description, answer });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-  
+
 
 exports.getScenarios = async (req, res) => {
-    try {
-        const scenarios = await Scenario.find().populate('department clinic multipleChoiceAnwsers questions.questionId');
-        res.status(200).json(scenarios);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  try {
+    const scenarios = await Scenario.find().populate('department clinic multipleChoiceAnwsers questions.questionId');
+    res.status(200).json(scenarios);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 exports.getScenariosByClinicId = async (req, res) => {
-    try {
-      const { clinicId } = req.params;
-      const scenarios = await Scenario.find({ clinic: clinicId })
-      res.status(200).json(scenarios);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  };
+  try {
+    const { clinicId } = req.params;
+    const scenarios = await Scenario.find({ clinic: clinicId })
+    res.status(200).json(scenarios);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-  exports.getScenarioById = async (req, res) => {
-    try {
-      const scenario = await Scenario.findById(req.params.id)
-        .populate({
-          path: 'department',
+exports.getScenarioById = async (req, res) => {
+  try {
+    const scenario = await Scenario.findById(req.params.id)
+      .populate({
+        path: 'department',
+        populate: {
+          path: 'testSteps',
+          model: 'Step',
           populate: {
-            path: 'testSteps',
-            model: 'Step',
-            populate: {
-              path: 'type',
-              model: 'ComponentType'
-            }
+            path: 'type',
+            model: 'ComponentType'
           }
-        });
-        
-      if (!scenario) {
-        return res.status(404).json({ error: 'Scenario not found' });
-      }
-  
-      res.status(200).json(scenario);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
+        }
+      });
+
+    if (!scenario) {
+      return res.status(404).json({ error: 'Scenario not found' });
     }
-  };
-  
+
+    res.status(200).json(scenario);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
 
 exports.updateScenario = async (req, res) => {
-    try {
-        const scenario = await Scenario.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-        if (!scenario) {
-            return res.status(404).json({ error: 'Scenario not found' });
-        }
-        res.status(200).json(scenario);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
+  try {
+    const scenario = await Scenario.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!scenario) {
+      return res.status(404).json({ error: 'Scenario not found' });
     }
+    res.status(200).json(scenario);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 
 exports.deleteScenario = async (req, res) => {
-    try {
-        const scenario = await Scenario.findByIdAndDelete(req.params.id);
-        if (!scenario) {
-            return res.status(404).json({ error: 'Scenario not found' });
-        }
-        res.status(200).json({ message: 'Scenario deleted' });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+  try {
+    const scenario = await Scenario.findByIdAndDelete(req.params.id);
+    if (!scenario) {
+      return res.status(404).json({ error: 'Scenario not found' });
     }
+    res.status(200).json({ message: 'Scenario deleted' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
